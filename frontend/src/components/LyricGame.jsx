e7a4809e9a82bee30e3df5514d68439c67cf3f80
Vishwas Kumawat <vishwaskumawat57@gmail.com>
2025-03-23 22:07:21 +0530

    updated API Base URL

--- frontend/src/components/LyricGame.jsx
+++ frontend/src/components/LyricGame.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './LyricGame.css';
 
-const API_BASE_URL = import.meta.env.API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_KEY;
 
 const LyricGame = () => {
   const [lyricSnippet, setLyricSnippet] = useState('');
